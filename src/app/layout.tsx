60537916e58a6fa02892d67e947b854adbbeebdb
airlorm <elormnizi@gmail.com>
2025-05-27 13:34:41 +0300

    feat(home): add title and welcome message with styled layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+          <main className="flex flex-col gap-8 row-start-2 items-center text-center">
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   );
